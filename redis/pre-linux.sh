116537658e7bb7c788696fe70858f0d620a8b69d
liutongqing <1355583269@qq.com>
2022-03-21 16:14:58 +0800

    update

---
+++ redis/pre-linux.sh
@@ -0,0 +1,3 @@
+mkdir -p /data/docker/data/redis/data
+mkdir -p /data/docker/data/redis/logs
+curl -sSl https://raw.githubusercontent.com/redis/redis/6.0/redis.conf -o redis.conf
---
+++ redis/pre-win.bat
@@ -0,0 +1,3 @@
+@REM mkdir -p /Users/ltq/data/redis/data
+@REM mkdir -p /Users/ltq/data/redis/logs
+@REM curl -sSl https://raw.githubusercontent.com/redis/redis/6.0/redis.conf -o redis.conf
