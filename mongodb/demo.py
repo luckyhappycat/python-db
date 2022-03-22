52e489f4cb53d7f7a62b80ed5496108e2c7e67e5
liutongqing <1355583269@qq.com>
2022-03-22 20:04:06 +0800

    update

--- mongodb/demo.py
+++ mongodb/demo.py
@@ -1,6 +1,7 @@
+import os
 import pymongo
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+client = pymongo.MongoClient("mongodb://172.17.50.89:27017/")
 
 db = client["pyframe-test"]
 print(db)
@@ -8,8 +9,61 @@ print(db)
 dblist = client.list_database_names()
 print(dblist)
 
-
-var = {"path": "/data"}
 collection = db["environ"]
-ret = collection.insert_one(var)
-print(ret.inserted_id)
+
+# find_one
+primary_key = "build-3"
+ret = collection.find_one(filter={"_id": primary_key})
+if ret is None:
+    print("{}不存在，准备插入".format(primary_key))
+    # insert_one
+    environment = {"_id": primary_key, "path": "/data", "version":"1.0.0"}
+    ret = collection.insert_one(environment)
+    print(ret.inserted_id)
+else:
+    print("{}存在".format(primary_key))
+
+# update
+collection.update_one()
+
+# delete
+
+
+
+# class Env:
+#     def __init__(self, ip : str="172.17.50.89", port: int=27017) -> None:
+#         self.ip = ip
+#         self.port = port
+#         self.client = pymongo.MongoClient("mongodb://{}:{}/".format(ip, port))
+#         self.db = client['pyframe-test']
+#         self.col = db["environ"]
+#         self.pipelineid = "pipelineid"
+#         self.pipeline_number = "1"
+#         # self.pipelineid = os.environ["pipelineid"]
+#         # self.pipeline_number = os.environ["pipeline_number"]
+
+#     def exist(self):
+#         ret = self.col.find_one({"_id": "{}_{}".format(self.pipelineid, self.pipeline_number)})
+#         if ret is None:
+#             return False
+#         else:
+#             return True
+
+#     def set(self, environment: dict):
+#         self.col.insert_one(environment)
+
+#     def save(self):
+#         pass
+
+#     def get(self, key: str):
+#         ret = self.col.find_one({"_id": "{}_{}".format(self.pipelineid, self.pipeline_number)})
+#         if ret is not None:
+#             return ret.get(key)
+#         else:
+#             environment = os.environ
+#             return environment.get(key)
+
+
+#     def load(self):
+#         ret = self.col.find_one({"_id": "{}_{}".format(self.pipelineid, self.pipeline_number)})
+#         return ret
