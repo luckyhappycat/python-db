ba4c3e292e7843caeedcf79f8782328083e3cbc4
liutongqing <1355583269@qq.com>
2022-06-21 21:26:52 +0800

    update redis

--- redis/pre-linux.sh
+++ redis/pre-linux.sh
@@ -1,3 +1,3 @@
-mkdir -p /data/docker/data/redis/data
-mkdir -p /data/docker/data/redis/logs
+mkdir -p /data/redis/data
+mkdir -p /data/redis/logs
 curl -sSl https://raw.githubusercontent.com/redis/redis/6.0/redis.conf -o redis.conf
